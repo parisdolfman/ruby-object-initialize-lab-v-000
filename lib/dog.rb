571e2ce231bd5e31f24f3e0d2d281dfb8a638473
Paris Dolfman <parisdolfman@gmail.com>
2018-01-24 21:07:13 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,8 @@
 class Dog
-  def initialize(dog_name)
-    @name = dog_name
-  end
 
-  def name=(dog_name)
-    @name = dog_name
-  end
+def initialize(name, breed)
+  @name = name
+  @breed = breed
+end
 
-  def name
-    @name
-  end
 end
