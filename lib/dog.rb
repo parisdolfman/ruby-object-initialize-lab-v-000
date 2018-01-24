8f79c83c0050fed172af7d1e15c8609a0aa7881d
Paris Dolfman <parisdolfman@gmail.com>
2018-01-24 21:08:36 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,4 +5,20 @@ def initialize(name, breed)
   @breed = breed
 end
 
+def name=(name)
+  @name = name
+end
+
+def name
+  @name
+end
+
+def breed=(breed)
+  @breed = breed
+end
+
+def breed
+  @breed
+end
+
 end
